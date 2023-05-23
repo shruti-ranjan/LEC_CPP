454e565a8c2bde7a392fb7e5e91fcddd1d7c8298
Shruti Ranjan <ranjanshruti22@gmail.com>
2023-05-23 18:41:22 +0545

    first chapter

--- 1_practice/1_ifelse.cpp
+++ 1_practice/1_ifelse.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 int main()
 {
-    int sex;
-    int balance;
-    int bonus;
-    int male = 0;
-    int female = 1;
-    if(sex==female)
-      if(balance>50000)
-       bonus = 5*balance;
+    int num1, num2, num3;
+    cout<<"Enter three integers:";
+    cin>>num1>>num2>>num3;
+    if(num1>num2)
+     if(num1>num3)
+      cout<<num1<<"Is the largest";
+      else
+       cout<<num3<<"Is the largest";
+    else 
+     if(num2>num3)
+      cout<<num2<<"Is the largest";
     else
-     balance = balance + bonus;
-       
+      cout<<num3<<"Is the largest";
+    system("pause");
+    return 0;
 
-      }
+}
