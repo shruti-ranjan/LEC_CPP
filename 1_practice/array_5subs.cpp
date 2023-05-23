cb99bb5db661b01bed7f62eaf1a0ee9b725c2844
Shruti Ranjan <ranjanshruti22@gmail.com>
2023-05-23 19:34:47 +0545

    first chapter

---
+++ 1_practice/array_5subs.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string sub[]={"OOP","TOC","EDC","ECT","EM"};
+    int marks [5];
+    int i,total;
+
+    for(i=0;i<5;i++)
+    {
+        cout<<"\nEnter the marks of subject :"<<sub[i]<<" : ";
+        cin>>marks[i];
+    }
+    for(i=0;i<5;i++)
+    {
+      total+=marks[i];
+    }
+    cout<<"\nTotal Marks:" <<total;
+    return 0; 
+    
+    }
