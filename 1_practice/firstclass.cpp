74ff0fc3fc3732a102cd4dcbd91fc45bd4a40401
Shruti Ranjan <ranjanshruti22@gmail.com>
2023-07-12 18:23:32 +0545

    practice

---
+++ 1_practice/firstclass.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Room
+{
+public:
+    int length, breadth, height;
+
+    void info()
+    {
+        cout << "Enter length,breadth,and height:";
+        cin >> length >> breadth >> height;
+    }
+    void calculateArea()
+    {
+        cout << "The area of the room is:" << length * breadth << endl;
+    }
+    void calculateVolume()
+    {
+        cout << "The volume of the room is:" << length * breadth * height;
+    }
+};
+int main()
+{
+    Room obj401;
+    obj401.info();
+    obj401.calculateArea();
+    obj401.calculateVolume();
+}
